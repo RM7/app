f8106926afb7d0708f3bb9c10c8e7602853ef895
c0nner1 <kf.koszarek@gmail.com>
2016-06-11 21:43:21 +0200

    map richtig

--- SchnellerVoran-B&B/app/src/main/java/de/jugendhackt/schnellervoran_bb/MapsLine2.java
+++ SchnellerVoran-B&B/app/src/main/java/de/jugendhackt/schnellervoran_bb/MapsLine2.java
@@ -41,20 +41,19 @@ public class MapsLine2 extends FragmentActivity implements OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    private static final LatLng V1 = new LatLng(50.974745,
-            7.014628);
-    private static final LatLng V2 = new LatLng(50.980109, 7.016961);
-    private static final LatLng V4 = new LatLng(50.977707, 7.032540);
-    private static final LatLng V3 = new LatLng(50.975564, 7.025623);
-    private static final LatLng V5 = new LatLng(50.976648, 7.035207);
-    private static final LatLng V6 = new LatLng(50.978585, 7.039700);
-    private static final LatLng V7 = new LatLng(50.982036, 7.043635);
-    private static final LatLng V8 = new LatLng(50.988029, 7.041970);
-    private static final LatLng V9 = new LatLng(50.988001, 7.041007);
-    private static final LatLng V10 = new LatLng(50.991857, 7.043104);
-    private static final LatLng V11 = new LatLng(50.994145, 7.040630);
-    private static final LatLng V12 = new LatLng(50.997205, 7.045466);
-    private static final LatLng V13 = new LatLng(50.998891, 7.034354);
+    private static final LatLng V1 = new LatLng(50.916820, 6.900012);
+    private static final LatLng V2 = new LatLng(50.918678, 6.902601);
+    private static final LatLng V3 = new LatLng(50.919712, 6.904642);
+    private static final LatLng V4 = new LatLng(50.924308, 6.907359);
+    private static final LatLng V5 = new LatLng(50.924156, 6.912056);
+    private static final LatLng V6 = new LatLng(50.925985, 6.915895);
+    private static final LatLng V7 = new LatLng(50.927947, 6.918456);
+    private static final LatLng V8 = new LatLng(50.930548, 6.922762);
+    private static final LatLng V9 = new LatLng(50.931121, 6.929169);
+    private static final LatLng V10 = new LatLng(50.933040, 6.934353);
+    private static final LatLng V11 = new LatLng(50.935530, 6.931992);
+    private static final LatLng V12 = new LatLng(50.936176, 6.939421);
+    private static final LatLng V13 = new LatLng(50.935778, 6.947712);
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -62,7 +61,7 @@ public class MapsLine2 extends FragmentActivity implements OnMapReadyCallback {
         // Add a marker in Sydney and move the camera
         mMap.addMarker(new MarkerOptions()
                 .position(new LatLng( 50.916820, 6.900012))
-                .title("Haltestelle")
+                .title("Endstation")
                 .snippet("Deckstein")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
         mMap.addMarker(new MarkerOptions()
@@ -122,7 +121,7 @@ public class MapsLine2 extends FragmentActivity implements OnMapReadyCallback {
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
         mMap.addMarker(new MarkerOptions()
                 .position(new LatLng( 50.935778, 6.947712))
-                .title("Haltestelle")
+                .title("Endstation")
                 .snippet("Neumarkt")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
         addLines();
